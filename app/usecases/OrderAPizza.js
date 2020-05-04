c6e67ce0e4952e5ab10c87739e7d5f39a96088a2
Etienne GIROT <egirot@octo.com>
2020-05-04 18:20:07 +0200

    Check on pizzeria existance

--- app/usecases/OrderAPizza.js
+++ app/usecases/OrderAPizza.js
@@ -1,9 +1,21 @@
 const PizzaOrderedEvent = require('../domain/PizzaOrderedEvent');
+const PizzeriaNotFoundEvent = require('../domain/PizzeriaNotFoundEvent');
 module.exports = {
     OrderAPizza: class OrderAPizza {
+        constructor(pizzeriaRepository) {
+            this.pizzeriaRepository = pizzeriaRepository;
+        }
         execute(command) {
+            const pizzeria = this.pizzeriaRepository.get(command.pizzeriaId);
+            if (pizzeria == null) {
+                return new PizzeriaNotFoundEvent();
+            }
             return new PizzaOrderedEvent();
         }
     },
-    OrderAPizzaCommand: class OrderAPizzaCommand{}
+    OrderAPizzaCommand: class OrderAPizzaCommand{
+        constructor(pizzeriaId) {
+            this.pizzeriaId = pizzeriaId;
+        }
+    }
 }
--- test/test.js
+++ test/test.js
@@ -1,17 +1,60 @@
 const expect = require('expect');
 const {OrderAPizza, OrderAPizzaCommand} = require('../app/usecases/OrderAPizza');
 const PizzaOrderedEvent = require('../app/domain/PizzaOrderedEvent');
+const PizzeriaNotFoundEvent = require('../app/domain/PizzeriaNotFoundEvent');
+const PizzeriaRepository = require('../app/infrastructure/PizzeriaRepository');
 
 describe('Order a pizza', function() {
-    it('should return a PizzaOrderedEvent', function() {
-        // given
-        const orderAPizza = new OrderAPizza();
-        const command = new OrderAPizzaCommand();
+    describe('When pizzeria is found', () => {
+        it('should return a PizzaOrderedEvent', () => {
+            // given
+            const pizzeriaRepository = new PizzeriaRepositoryForTest();
+            const orderAPizza = new OrderAPizza(pizzeriaRepository);
+            const command = new OrderAPizzaCommand(1);
 
-        // when
-        const event = orderAPizza.execute(command);
+            // when
+            const event = orderAPizza.execute(command);
 
-        // then
-        expect(event).toBeInstanceOf(PizzaOrderedEvent);
+            // then
+            expect(event).toBeInstanceOf(PizzaOrderedEvent);
+        });
     });
+    describe('When pizzeria is not found', () => {
+        it('should return a PizzeriaNotFoundEvent', () => {
+            // given
+            const pizzeriaRepository = new PizzeriaRepositoryForTest();
+            const orderAPizza = new OrderAPizza(pizzeriaRepository);
+            const command = new OrderAPizzaCommand(-1);
+
+            // when
+            const event = orderAPizza.execute(command);
+
+            // then
+            expect(event).toBeInstanceOf(PizzeriaNotFoundEvent);
+        });
+    });
+
+    // TODO
+    // When customer exists
+    // When customer not exists
+    // When pizza is on the menu
+    // When pizza is not on the menu
+    // When enough ingredients
+    // When not enough ingredients
+    // Make pizza (update ingredients stocks)
+    // When payment mean is accepted by restaurant
+    // When payment mean is not accepted by restaurant
+    // When enough money to pay
+    // When not enough money to pay
+    // Pay (update pizzeria and owner account balances)
+    // Register order
 });
+
+class PizzeriaRepositoryForTest extends PizzeriaRepository {
+    #pizzerie = {
+        1: {name: "Da Marco"}
+    }
+    get(id) {
+        return this.#pizzerie[id];
+    }
+}
