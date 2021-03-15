5e80e9461addaf1c224140e6a2d5943908a490ca
Vincent Deleplanque <vdeleplanque@octo.com>
2021-03-15 09:49:35 +0100

    Add ts folder

---
+++ ts/src/pizza.ts
@@ -0,0 +1,3 @@
+export const pizza = {
+    exists: () => true
+}
---
+++ ts/test/pizza.test.ts
@@ -0,0 +1,7 @@
+import { pizza } from '../src/pizza'
+
+describe('something', () => {
+    it('does this', () => {
+        expect(pizza.exists()).toEqual(true)
+    })
+})
